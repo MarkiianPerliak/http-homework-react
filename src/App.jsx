e78d19795074cb7958865d483e2c5069a752abf5
MarkiianPerliak <122627880+MarkiianPerliak@users.noreply.github.com>
2026-02-13 18:07:34 +0200

    done

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css';
+import { ImageGallery } from './ImageGallery.jsx';
+
+export const App = () => {
+  return (
+    <div className="App">
+      <ImageGallery />
+    </div>
+  );
+}
